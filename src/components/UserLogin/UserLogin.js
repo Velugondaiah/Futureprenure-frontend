2d5898417634aa17a51a09d4315313e820b83770
Kushvanth <kushvanthyalamanchi2004@gmail.com>
2025-08-28 09:22:46 +0530

    now

--- src/components/UserLogin/UserLogin.js
+++ src/components/UserLogin/UserLogin.js
@@ -28,7 +28,7 @@ class LoginForm extends Component {
     const {username, password} = this.state
     
     const userDetails = {username, password}
-    const url = 'https://backend-diagno-1.onrender.com/api/login'
+    const url = 'http://localhost:3009/api/login'
                 
     try {
       const response = await fetch(url, {
